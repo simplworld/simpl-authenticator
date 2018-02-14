623a0a8c3c1f494e80a6fd530127244553e0d01c
Jane Eisenstein <ejane@wharton.upenn.edu>
2018-02-14 15:09:52 -0500

    Bump version: 0.0.5 → 0.0.6

--- authenticator/__init__.py
+++ authenticator/__init__.py
@@ -2,7 +2,7 @@ from twisted.internet.defer import inlineCallbacks
 
 from autobahn.twisted.wamp import ApplicationSession
 
-_version = "0.0.5"
+_version = "0.0.6"
 __version__ = VERSION = tuple(map(int, _version.split('.')))
 
 
