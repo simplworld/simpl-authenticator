aa8766df5b3ec43dcb9c9c038df282f174dd28c8
Frank Wiles <frank@revsys.com>
2018-09-11 15:36:40 -0500

    Bump version: 0.7.0 → 0.7.1

--- authenticator/__init__.py
+++ authenticator/__init__.py
@@ -2,7 +2,7 @@ from twisted.internet.defer import inlineCallbacks
 
 from autobahn.twisted.wamp import ApplicationSession
 
-_version = "0.7.0"
+_version = "0.7.1"
 __version__ = VERSION = tuple(map(int, _version.split('.')))
 
 
